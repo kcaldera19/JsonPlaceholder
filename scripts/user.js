0216283ef74e79fbc41235d6c3c6539874f06f0f
Katalina Caldera <katalinacaldera19@gmail.com>
2024-06-04 16:03:18 -0500

    added the info for the table

--- scripts/user.js
+++ scripts/user.js
@@ -1,6 +1,46 @@
 "use strict"
 
-window.onload = ()=>{
+window.onload = () => {
     console.log("I am working here")
+    getUser();
 
 }
+
+async function getUser() {
+    try {
+
+        let response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        if (!response.ok) {
+            throw new Error("could not fetch resources");
+        }
+
+
+        let data = await response.json();
+        let usersTableBody = document.getElementById("usersTableBody");
+        usersTableBody.innerHTML = "";
+
+        data.forEach((users) => {
+            usersTableBody.innerHTML += `
+            ${users.id}
+            ${users.name} 
+            ${users.username}
+            ${users.email}
+            ${users.phone}
+            ${users.website}
+           
+           
+            `;
+
+        });
+
+
+
+
+
+    }
+    catch (error) {
+        console.error(error)
+
+    }
+};
+
